63c571b99b8951114898f0ed16ee7e625d6df772
OnionPowder01 <autonatione36@gmail.com>
2023-02-22 13:52:47 +0200

    url validation

--- Shortly/src/App.js
+++ Shortly/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 import Shorten from "./components/Shorten";
--- Shortly/src/components/InputForm.jsx
+++ Shortly/src/components/InputForm.jsx
@@ -1,31 +1,65 @@
 import React, { useState } from "react";
+import validator from 'validator'
 
-function InputForm({ setInputValue }) {
+function InputForm({ setInputValue, inputValue }) {
     const [value, setValue] = useState('')
 
     const handleClick = () => {
         setInputValue(value);
         setValue("");
     }
+    
+  // Validate a URL
+const validate = (value) => {
+  return validator.isURL(value)
+
+}
+
+const [errMsg, setErrMsg] = useState('')
+const [customClass, setCustomClass] = useState('')
+
+function formSubmit(e) {
+  e.preventDefault()
+
+  if (value === '') {
+      setErrMsg('Please enter something')
+      setCustomClass('border-red')
+  } else if (validate(value) === false) {
+      setErrMsg('Please enter a valid URL')
+      setCustomClass('border-red')
+  } else {
+      setErrMsg('')
+      setCustomClass('')
+    alert('Success, your link is under the input field')
+  }
+}
+
+    console.log(validate(value))
+    console.log(errMsg)
     return (
         <form
           action=""
           id="link-form"
           className="relative flex flex-col w-full p-10 -mt-20 space-y-4 bg-darkViolet rounded-lg md:flex-row md:space-y-0 md:space-x-3"
+          onClick={formSubmit}
+          
         >
           <input
             type="text"
-            className="flex-1 p-3 border-2 rounded-lg placeholder-yellow-500 focus:outline-none"
+            className={`flex-1 p-3 border-2 rounded-lg placeholder-yellow-500 focus:outline-none ${customClass}`}
             placeholder="Shorten a link here"
             id="link-input"
             value={value}
             onChange={(e) => setValue(e.target.value)}
           />
-          <button type="button" onClick={handleClick} className="px-10 py-3 text-white bg-cyan rounded-lg hover:bg-cyanLight focus:outline-none md:py-2">
+          <button onClick={handleClick} className="px-10 py-3 text-white bg-cyan rounded-lg hover:bg-cyanLight focus:outline-none md:py-2">
             Shorten It
           </button>
+          {/* Error Message */}
+          <div id="err-message" className="absolute left-7 bottom-3 text-red italic">{errMsg}</div>
         </form>
     )
 }
+
 
 export default InputForm
